ac957062554666c55f7cc589b405fac860da6b1d
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-11-13 11:34:07 -0500

    sanitize config (no secrets)

--- lab02/02/Solution/func/obj/Debug/net8.0/func.AssemblyInfo.cs
+++ lab02/02/Solution/func/obj/Debug/net8.0/func.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("func")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06ef90be02d672d5251fbd3752a8e9977f5a5c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a8fd49dccdea685605906a17d54fb45a1a0f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("func")]
 [assembly: System.Reflection.AssemblyTitleAttribute("func")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
